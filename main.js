009e3ac6cfccfd864d545706f506ccdc980a767b
Satya Priya Ambati <155984169+SatyaPriyaAmbati@users.noreply.github.com>
2024-02-01 18:29:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer", "Backend Developer","Java Developer","Full-stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
